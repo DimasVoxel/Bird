a3092dec18c801c8e9bc8c19438011d1e43c0739
DimiWin <kimeldim@gmail.com>
2024-01-02 21:25:56 +0100

    bird spawner

---
+++ spawnbird.lua
@@ -0,0 +1,12 @@
+function init()
+    for i=1,40 do 
+        local min,max = GetBodyBounds(GetWorldBody())
+        QueryRequire("physical static large")
+        local pos = Vec(math.random(min[1],max[1]),math.random(min[2],max[2]),math.random(min[3],max[3]))
+        local hit,dist = QueryRaycast(pos,Vec(0,-1,0),200,0,false)
+        if hit then
+            local hitpoint = VecAdd(pos,VecScale(Vec(0,-1,0),dist-3))
+            Spawn("MOD/Birds/pigon.xml",Transform(hitpoint),false)
+        end
+    end
+end
