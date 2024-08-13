78777e0a72df3652fc016475c8e21845aec39195
DimiWin <kimeldim@gmail.com>
2024-08-13 14:32:07 +0200

    Misc changes and added new birds to spawn menu due to player request

--- main.lua
+++ main.lua
@@ -1,3 +1,10 @@
 function init()
-    Spawn('<script pos="0.0 0.2 0.0" file="MOD/pathFInderLoadingScreen.lua"/>',Transform())
+    once = false
+end
+
+function tick()
+    if once == false then 
+        once = true 
+        Spawn('<script pos="0.0 0.2 0.0" file="MOD/pathFInderLoadingScreen.lua"/>',Transform())
+    end
 end
--- spawnbird.lua
+++ spawnbird.lua
@@ -1,23 +1,29 @@
 function init()
 
-    spawnInit()
-    counter = 0
-    for i=1,30 do 
-        for retry=0,20 do 
-            local min,max = GetBodyBounds(GetWorldBody())
-            QueryRequire("physical static large")
-            local pos = Vec(math.random(min[1],max[1]),math.random(min[2],max[2]),math.random(min[3],max[3]))
-            local hit,dist = QueryRaycast(pos,Vec(0,-1,0),200,0,false)
-            local hitpoint = VecAdd(pos,VecScale(Vec(0,-1,0),dist-3))
-            if hit and IsPointInWater(hitpoint) == false then
-                local XML = getrandom(birds.default.spawnTable,birds.default.totalPercent)
-                Spawn(XML,Transform(hitpoint),false)
-                counter = counter + 1
-                break
+once = false 
+end
+
+function tick()
+    if once == false then
+        once = true
+        spawnInit()
+        counter = 0
+        for i=1,30 do 
+            for retry=0,20 do 
+                local min,max = GetBodyBounds(GetWorldBody())
+                QueryRequire("physical static large")
+                local pos = Vec(math.random(min[1],max[1]),math.random(min[2],max[2]),math.random(min[3],max[3]))
+                local hit,dist = QueryRaycast(pos,Vec(0,-1,0),200,0,false)
+                local hitpoint = VecAdd(pos,VecScale(Vec(0,-1,0),dist-3))
+                if hit and IsPointInWater(hitpoint) == false then
+                    local XML = getrandom(birds.default.spawnTable,birds.default.totalPercent)
+                    Spawn(XML,Transform(hitpoint),false)
+                    counter = counter + 1
+                    break
+                end
             end
         end
     end
-    DebugPrint(counter)
 end
 
 function spawnInit()
@@ -32,12 +38,25 @@ function spawnInit()
     birds = {
         default = {
             spawnTable = {
-                { "MOD/Birds/pigon.xml", 50 },
-                { "MOD/Birds/tit.xml", 40 },
-                { "MOD/Birds/sparrow.xml", 40 },
-                { "MOD/Birds/AmericanRobbin.xml", 40 },
-                { "MOD/Birds/magpie.xml", 20 },
+                { "MOD/Birds/pigon.xml", 30 },
+                { "MOD/Birds/tit.xml", 25 },
+                { "MOD/Birds/bluetit.xml", 25 },
+                { "MOD/Birds/sparrow.xml", 25 },
+                { "MOD/Birds/AmericanRobbin.xml", 20 },
+                { "MOD/Birds/Swift.xml",17},
+                { "MOD/Birds/House_martin.xml",15},
+                { "MOD/Birds/magpie.xml", 15 },
+                { "MOD/Birds/Robin.xml",15},
                 { "MOD/Birds/crow.xml", 15 },
+                { "MOD/Birds/seagull.xml",15},
+                { "MOD/Birds/duck.xml",15},
+                { "MOD/Birds/Serin.xml",10},
+                { "MOD/Birds/GoldFinch.xml",10},
+                { "MOD/Birds/Jay.xml",10},
+                { "MOD/Birds/kingfisher.xml",8},
+                { "MOD/Birds/Green_Woodpecker.xml",6},
+                { "MOD/Birds/Red_Velvet.xml",4},
+                { "MOD/Birds/pukeko.xml", 3 },
             },
             totalPercent = 0
         },
